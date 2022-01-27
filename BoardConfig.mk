2f2649f31ef7185f7827797a3fa0d22e0de4f1b1
Edwin Moquete <edwinmmoquete@gmail.com>
2022-01-27 00:55:18 +0000

    X01BD: Implement QTI AIDL vibrator HAL
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,6 +147,7 @@ TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_IGNORE_MISC_WIPE_DATA := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_EXCLUDE_TWRPAPP := true
+TW_SUPPORT_INPUT_AIDL_HAPTICS := true
 
 # Notch
 TW_Y_OFFSET := 88
