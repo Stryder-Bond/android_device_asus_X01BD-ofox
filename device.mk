325515679ffd4bad0972be86dbbf58d0b3673e7d
Rohit Tiwari <rohitttiwari52@gmail.com>
2024-01-14 03:16:54 +0530

    X01BD: Build & enable fastbootd
    Signed-off-by: Rohit Tiwari <rohitttiwari52@gmail.com>

--- device.mk
+++ device.mk
@@ -11,6 +11,18 @@ PRODUCT_SHIPPING_API_LEVEL := 27
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
 
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.boot.dynamic_partitions=true \
+	ro.boot.dynamic_partitions_retrofit=true
+
+# Fastbootd
+TW_INCLUDE_FASTBOOTD := true
+PRODUCT_PACKAGES += \
+    fastbootd
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.fastbootd.available=true
+
 # QCOM Decryption
 PRODUCT_PACKAGES += \
     qcom_decrypt \
